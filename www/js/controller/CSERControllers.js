726075a4532fa9602f1c9cae453f075f5ec0674d
johnsing <johnsing.he@flex.com>
2019-03-12 16:55:33 +0800

    backup

--- www/js/controller/CSERControllers.js
+++ www/js/controller/CSERControllers.js
@@ -301,7 +301,8 @@ angular.module('evaluationApp.CSERControllers', [])
     }
     InitInfo();
   })
-  .controller('CSERActivityCenterFaqCtrl', function ($scope, $rootScope, $state, $ionicHistory, commonServices) 
+  .controller('CSERActivityCenterFaqCtrl', 
+      function ($scope, $rootScope, $state, $ionicHistory, commonServices) 
   {
       //娱乐设施常见问题
       function GetList(paras) {
@@ -314,78 +315,241 @@ angular.module('evaluationApp.CSERControllers', [])
       var paras = commonServices.getBaseParas();
       paras.keyword = "宿舍";
       GetList(paras);
-    })
-    .controller('DormSuggestCtrl', function ($scope, $rootScope, $state, $ionicHistory, $ionicPopup,
-      commonServices, CacheFactory, alertService, duplicateSubmitServices) 
-    {
-      //建议箱
-      var baseInfo = commonServices.getBaseParas();
-      $scope.hisSuggest = [];
+  })
+  .controller('CSERActivityCenterRepairCtrl', function ($scope, $rootScope, $state, $ionicHistory, $ionicPopup,
+                                        commonServices, CacheFactory, alertService, duplicateSubmitServices,
+                                        PicServices, UrlServices) 
+  {
+    //娱乐设施报修
+    var baseInfo = commonServices.getBaseParas();
+    $scope.canSubmit = false;
+    $scope.model = {
+      SubmitGuid: duplicateSubmitServices.genGUID(),
+      CName: baseInfo.CName,
+      WorkdayNO: baseInfo.WorkdayNO,
+      MobileNo: baseInfo.MobileNo,
+      DormArea: null,
+      DormAddress: null,
+      RepairTime: moment().add(1, 'h').minute(0).toDate(),
+      DeviceType: null,
+      RepairDesc: null
+    };
 
-      function InitInfo() {
-        var url = commonServices.getUrl("DormManageService.ashx", "GetDormSuggest");
-        var paras = {
-          WorkdayNO: baseInfo.WorkdayNO
-        };
-        commonServices.submit(paras, url).then(function (resp) {
-          if (resp) {
-            if (resp.success) {
-              $scope.hisSuggest = resp.list;
-            }
-            $scope.dormAreas = JSON.parse(resp.data);
-          }
-        });
-      }
-      InitInfo();
-
-      $scope.model = {
-        SubmitGuid: duplicateSubmitServices.genGUID(),
-        CName: baseInfo.CName,
+    function InitInfo() {
+      var url = commonServices.getUrl("DormManageService.ashx", "GetDormCheckInInfo");
+      var paras = {
         WorkdayNO: baseInfo.WorkdayNO,
-        MobileNo: baseInfo.MobileNo,
-        Suggest: "",
-        DormArea: null
+        Extra: "GetRepairType"
       };
-      $scope.GetSuggest = function () {
-        var txt = $.trim($scope.model.Suggest);
-        return txt;
-      };
-
-      $scope.isSumbiting = false;
-      $scope.Submit = function () {
-        $scope.isSumbiting = true;
-        if (!$scope.model.DormArea) {
-          alertService.showAlert("请选择宿舍区!");
-          $scope.isSumbiting = false;
+      commonServices.submit(paras, url).then(function (resp) {
+        if (resp && resp.obj) {
+          var curCheckInfo = resp.obj;
+          if (!resp.success) {
+            /* 不作强制检查
+            alertService.showAlert("报修失败："+resp.message);
+            $ionicHistory.goBack();
+            */
+            $scope.model.DormAddress = '';
+            $scope.canSubmit = true;
+          } else {
+            var checkInInfo = curCheckInfo.CheckInfo;
+            $scope.model.DormArea = checkInInfo.DormAreaID;
+            $scope.model.DormAddress = checkInInfo.DormAddress;
+            $scope.canSubmit = true;
+          }
+          var arr = JSON.parse(resp.data);
+          $scope.deviceTypes = arr;
+          $scope.dormAreas = curCheckInfo.DormAreas;
+        } else {
+          alertService.showAlert("获取报修信息失败，请稍候再试!");
           return;
         }
-        var sugg = $scope.GetSuggest();
-        if (sugg.length < 3) {
-          alertService.showAlert("请填写你的建议!");
-          $scope.isSumbiting = false;
-          return;
-        }
+      });
+    }
+    InitInfo();
 
-        $scope.model.Suggest = sugg;
-        var paras = $scope.model;
-        var url = commonServices.getUrl("DormManageService.ashx", "SubmitDormSuggest");
+    $scope.imgs = [];
+    $scope.SelPic = function (bCamera) {
+      PicServices.selectImage(function (pic) {
+        PicServices.resizeImage(1024, pic, function (sdata) {
+          $scope.imgs.push(sdata);
+        });
+      }, bCamera);
+    };
+
+    var Reset = function () {
+      $scope.imgs = [];
+    };
+    Reset();
+    $scope.Reset = Reset;
+
+    $scope.isSumbiting = false;
+    $scope.Submit = function () {
+      $scope.isSumbiting = true;
+
+      var sTemp = $.trim($scope.model.MobileNo);
+      $scope.model.MobileNo = sTemp;
+      if (!sTemp || sTemp.length < 5) {
+        alertService.showAlert("请提供联系电话!");
+        $scope.isSumbiting = false;
+        return;
+      }
+      if (!$scope.model.DormArea) {
+        alertService.showAlert("请选择宿舍区!");
+        $scope.isSumbiting = false;
+        return;
+      }
+      sTemp = $.trim($scope.model.DormAddress);
+      if (isEmptyString(sTemp)) {
+        alertService.showAlert("请提供具体地址(例：北厂宿舍77栋A77房Z床)!");
+        $scope.isSumbiting = false;
+        return;
+      } else {
+        var sDormArea = $.trim($("#idDormArea option:selected").text());
+        if (0 != sTemp.indexOf(sDormArea)) {
+          sTemp = sDormArea + sTemp;
+        }
+        $scope.model.DormAddress = sTemp;
+      }
+      sTemp = $.trim($scope.model.RepairTime);
+      if (isEmptyString(sTemp)) {
+        alertService.showAlert("请填写维修时间!");
+        $scope.isSumbiting = false;
+        return;
+      }
+      sTemp = $.trim($scope.model.DeviceType);
+      if (isEmptyString(sTemp)) {
+        alertService.showAlert("请要维修的设备类型!");
+        $scope.isSumbiting = false;
+        return;
+      }
+      sTemp = $.trim($scope.model.RepairDesc);
+      $scope.model.RepairDesc = sTemp;
+      // if (isEmptyString(sTemp)) {
+      //     alertService.showAlert("请填写报修内容!");
+      //     $scope.isSumbiting = false;
+      //     return;     
+      // }
+
+      var paras = $scope.model;
+      if (!$scope.imgs || !$scope.imgs.length) {
         try {
-          commonServices.submit(paras, url).then(function (resp) {
-            if (resp.success) {
-              var msg = $rootScope.Language.dormManage.suggestSucc;
-              alertService.showAlert(msg);
-              $ionicHistory.goBack();
-            } else {
-              alertService.showAlert(resp.message);
-              $ionicHistory.goBack();
-            }
-          });
+          DoSubmit(paras);
         } finally {
           $scope.isSumbiting = false;
         }
-      };
+      } else {
+        alertService.showOperating('Processing...');
+        var url = commonServices.getUrl("UploadService.ashx", "");
+        UrlServices.uploadImages('DormRepair', '宿舍报修', $scope.imgs, url, function (resp) {
+            alertService.hideOperating();
+            if (resp) {
+              if (resp.success) {
+                paras.ImageBatchNo = resp.obj;
+                try {
+                  DoSubmit(paras);
+                } catch (e) {
+                  console.log(e);
+                }
+              } else {
+                alertService.showAlert("上传图片失败, " + resp.message);
+              }
+            } else {
+              alertService.showAlert("上传图片失败!");
+            }
+            $scope.isSumbiting = false;
+          },
+          function (msg) {
+            alertService.showAlert("上传图片失败, " + msg);
+          });
+      }
+    };
 
-    })
+    function DoSubmit(paras) {
+      var url = commonServices.getUrl("DormManageService.ashx", "SubmitRepairDorm");
+      commonServices.submit(paras, url).then(function (resp) {
+        if (resp.success) {
+          var msg = $rootScope.Language.dormManage.repairDormSucc;
+          alertService.showAlert(msg);
+          $ionicHistory.goBack();
+        } else {
+          alertService.showAlert(resp.message);
+          $ionicHistory.goBack();
+        }
+      });
+    }
+  })
+  .controller('DormSuggestCtrl', function ($scope, $rootScope, $state, $ionicHistory, $ionicPopup,
+    commonServices, CacheFactory, alertService, duplicateSubmitServices) 
+  {
+    //建议箱
+    var baseInfo = commonServices.getBaseParas();
+    $scope.hisSuggest = [];
+
+    function InitInfo() {
+      var url = commonServices.getUrl("DormManageService.ashx", "GetDormSuggest");
+      var paras = {
+        WorkdayNO: baseInfo.WorkdayNO
+      };
+      commonServices.submit(paras, url).then(function (resp) {
+        if (resp) {
+          if (resp.success) {
+            $scope.hisSuggest = resp.list;
+          }
+          $scope.dormAreas = JSON.parse(resp.data);
+        }
+      });
+    }
+    InitInfo();
+
+    $scope.model = {
+      SubmitGuid: duplicateSubmitServices.genGUID(),
+      CName: baseInfo.CName,
+      WorkdayNO: baseInfo.WorkdayNO,
+      MobileNo: baseInfo.MobileNo,
+      Suggest: "",
+      DormArea: null
+    };
+    $scope.GetSuggest = function () {
+      var txt = $.trim($scope.model.Suggest);
+      return txt;
+    };
+
+    $scope.isSumbiting = false;
+    $scope.Submit = function () {
+      $scope.isSumbiting = true;
+      if (!$scope.model.DormArea) {
+        alertService.showAlert("请选择宿舍区!");
+        $scope.isSumbiting = false;
+        return;
+      }
+      var sugg = $scope.GetSuggest();
+      if (sugg.length < 3) {
+        alertService.showAlert("请填写你的建议!");
+        $scope.isSumbiting = false;
+        return;
+      }
+
+      $scope.model.Suggest = sugg;
+      var paras = $scope.model;
+      var url = commonServices.getUrl("DormManageService.ashx", "SubmitDormSuggest");
+      try {
+        commonServices.submit(paras, url).then(function (resp) {
+          if (resp.success) {
+            var msg = $rootScope.Language.dormManage.suggestSucc;
+            alertService.showAlert(msg);
+            $ionicHistory.goBack();
+          } else {
+            alertService.showAlert(resp.message);
+            $ionicHistory.goBack();
+          }
+        });
+      } finally {
+        $scope.isSumbiting = false;
+      }
+    };
+
+  })
 
 ///////////////////////////////////////
 ;
